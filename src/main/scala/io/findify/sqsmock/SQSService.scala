c04a7869fee996b22f3b33160d43aeb51ba4447d
Roman Grebennikov <grebennikov.roman@gmail.com>
2016-03-31 15:24:38 +0300

    hide 'incorrect header' warnings from akka-http

--- src/main/scala/io/findify/sqsmock/SQSService.scala
+++ src/main/scala/io/findify/sqsmock/SQSService.scala
@@ -7,6 +7,7 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import com.typesafe.config.ConfigFactory
 import io.findify.sqsmock.actions.{CreateQueueWorker, ReceiveMessageWorker, SendMessageWorker}
 import io.findify.sqsmock.messages._
 import io.findify.sqsmock.model.{Message, Queue}
@@ -14,13 +15,14 @@ import io.findify.sqsmock.model.{Message, Queue}
 import scala.collection.mutable
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-
+import scala.collection.JavaConversions._
 
 /**
   * Created by shutty on 3/29/16.
   */
 class SQSService(port:Int, account:Int = 1) {
-  implicit val system = ActorSystem.create("sqsmock")
+  val config = ConfigFactory.parseMap(Map("akka.http.parsing.illegal-header-warnings" -> "off"))
+  implicit val system = ActorSystem.create("sqsmock", config)
   def start():Unit = {
     val log = Logger(system.getClass, "sqs_client")
     implicit val mat = ActorMaterializer()
